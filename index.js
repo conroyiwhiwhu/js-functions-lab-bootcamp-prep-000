4de0695a445b164611a211827f3b110cdf3b8e11
Conroy Iwhiwhu <conroy.iwhiwhu@gmail.com>
2018-02-21 21:29:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
-function happyHolidaysTo(name){
+function happyHolidaysTo(name_of_person){
   return `Happy ${holiday}, ${name}!`
 }
-function happyHolidaysTo(holiday, name){
+function happyHolidaysTo(holiday_name, name_of_person){
   return "Happy ${holiday}, ${name}!"
 }
-function holidayCountdown(holiday, days) {
+function holidayCountdown(holiday_name, number_of_days) {
   return `It's ${days} days until ${holiday}!`
 }
