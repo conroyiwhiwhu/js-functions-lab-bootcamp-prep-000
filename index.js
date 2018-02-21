8d4981b6ade5d9a16df4e2047388c3b88cf97055
Conroy Iwhiwhu <conroy.iwhiwhu@gmail.com>
2018-02-21 21:23:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy ${holiday}, ${name}!`
 }
+function happyHolidaysTo(holiday,name){
+  return "Happy ${holiday}, ${name}!"
+}
